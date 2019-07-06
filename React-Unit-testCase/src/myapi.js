6175fbc0997efe72ad605aae5df9fc7975ded98b
Rahul Kumar13 <rahul.kumar13@synapse.com>
2019-07-06 22:24:38 +0530

    removing the test cases project from example list

--- React-Unit-testCase/src/myapi.js
+++
@@ -1,6 +0,0 @@
-export  function getALL(){
-    return fetch("https://jsonplaceholder.typicode.com/posts")
-          .then(res=>{return res.json()})
-          .then(json=>json)
-          .catch(err=>err)  
- }
